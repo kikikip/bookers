369aef52bb5fa801afe476b4a9a2e1fa7718cfea
kikikip <135789408+kikikip@users.noreply.github.com>
2023-06-21 11:50:39 +0000

    [in the middle]bookers 6/21 20:48

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,5 @@
 class BooksController < ApplicationController
+
   def index
     @book = Book.new
     @books = Book.all
@@ -6,11 +7,15 @@ class BooksController < ApplicationController
 
   def create
     # １.&2. データを受け取り新規登録するためのインスタンス作成
-    book = Book.new(book_params)
+    @book = Book.new(book_params)
     # 3. データをデータベースに保存するためのsaveメソッド実行
-    book.save
-    # 4. トップ画面へリダイレクト
-    redirect_to "/books/#{book.id}"
+    if @book.save
+      # 4. トップ画面へリダイレクト
+      redirect_to "/books/#{book.id}"
+    else
+      @books = Book.all
+      render :index
+    end
   end
 
   def show
@@ -28,7 +33,9 @@ class BooksController < ApplicationController
   end
 
   def destroy
-
+    book = Book.find(params[:id])  # データ（レコード）を1件取得
+    book.destroy  # データ（レコード）を削除
+    redirect_to '/books'  # 投稿一覧画面へリダイレクト
   end
 
   private
